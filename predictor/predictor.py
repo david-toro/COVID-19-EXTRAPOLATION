fadb1ae0d59aa933ed24662dd1759a0887faac6e
David Toro <davidtoromeneses@gmail.com>
2020-02-13 02:40:05 -0300

    get and plot data in jupyter notebook

---
+++ predictor/predictor.py
@@ -0,0 +1,20 @@
+
+
+class Predictor:
+    def __init__(self, d):
+        self.d = d
+
+    def polynomial_regression(self):
+        pass
+
+    def exponential_model(self):
+        pass
+
+    def lagrange_polynomial(self):
+        pass
+
+    def logistic_model(self):
+        pass
+
+    def next_day(self):
+        pass
